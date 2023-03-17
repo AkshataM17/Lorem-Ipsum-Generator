1a9aa3ca58d6009f7dd20c0b0efddfe2fafa20c4
akshata <makshata403@gmail.com>
2023-03-17 18:54:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import data from './data';
 
-function App() {
+const App = () => {
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(count);
+    let amount = parseInt(count);
+    if(count <= 0){
+      amount = 1;
+    }
+    if(count > 8){
+     amount = 8;
+    }
+    setText(data.slice(0, amount));
+  }
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Generate Lorem Ipsum Paragraphs</h1>
+      <h3>Enter the number of paragraphs you want to get</h3>
+      <form action="" onSubmit={handleClick}>
+      <label htmlFor="lorem-ipsum">Paragraph : </label>
+      <input 
+        type="number" 
+        name='lorem-ipsum' 
+        id='lorem-ipsum'
+        value = {count}
+        onChange={handleChange}
+        />
+        <button type='submit'>Submit</button>
+    </form>
+    <article>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>
+      })}
+    </article>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
